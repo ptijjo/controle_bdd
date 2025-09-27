cfe4d69bf6bda938c14df0226afee9f6720a4ac0
Joseph Bangoura <ptijjo@hotmail.com>
2025-09-27 18:14:32 +0200

    pm2 en marche

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,11 @@
 /**
  * @description pm2 configuration file.
  * @example
- *  production mode :: pm2 start ecosystem.config.js --only prod
- *  development mode :: pm2 start ecosystem.config.js --only dev
  */
  module.exports = {
   apps: [
     {
-      name: 'prod', // pm2 start App name
+      name: 'backform', // pm2 start App name
       script: 'dist/server.js',
       exec_mode: 'cluster', // 'cluster' or 'fork'
       instance_var: 'INSTANCE_ID', // instance variable
@@ -20,15 +18,16 @@
       output: './logs/access.log', // pm2 log file
       error: './logs/error.log', // pm2 error log file
       env: { // environment variable
-        PORT: 3000,
+        PORT: 8585,
         NODE_ENV: 'production',
       },
     },
     {
-      name: 'dev', // pm2 start App name
-      script: 'ts-node', // ts-node
-      args: '-r tsconfig-paths/register --transpile-only src/server.ts', // ts-node args
+      name: 'frontform', // pm2 start App name
+      script: 'node_modules/.bin/next', // ts-node
+      args: 'start -p 3010', // ts-node args
       exec_mode: 'cluster', // 'cluster' or 'fork'
+      cwd: '/var/www/vhosts/cellulenoire.fr/controle/front',
       instance_var: 'INSTANCE_ID', // instance variable
       instances: 2, // pm2 instance count
       autorestart: true, // auto restart if process crash
@@ -38,20 +37,10 @@
       merge_logs: true, // if true, stdout and stderr will be merged and sent to pm2 log
       output: './logs/access.log', // pm2 log file
       error: './logs/error.log', // pm2 error log file
-      env: { // environment variable
-        PORT: 3000,
-        NODE_ENV: 'development',
+      env_production: { // environment variable
+        PORT: 3010,
+        NODE_ENV: 'production',
       },
     },
   ],
-  deploy: {
-    production: {
-      user: 'user',
-      host: '0.0.0.0',
-      ref: 'origin/master',
-      repo: 'git@github.com:repo.git',
-      path: 'dist/server.js',
-      'post-deploy': 'npm install && npm run build && pm2 reload ecosystem.config.js --only prod',
-    },
-  },
 };
